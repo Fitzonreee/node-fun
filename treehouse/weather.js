0a2522fc1a57766d403fd13b786453a0ff7a1f5e
Fitzonreee <kevin.fitzhenry@createthenext.com>
2017-03-08 15:35:54 -0800

    Completed weather app without error handling

--- treehouse/weather.js
+++ treehouse/weather.js
@@ -2,35 +2,34 @@ const https = require('https');
 const secrets = require('../secrets.js');
 
 // Function to print message to console
-function printMessage(loc, temp) {
-  const message = `Current temperature in ${loc} is ${temp}`;
+function printMessage(weather) {
+  const message = `Current temperature in ${weather.current_observation.display_location.full} is ${weather.current_observation.temp_f}F`;
   console.log(message);
 }
 // printMessage('Los Gatos','64.0F');
-console.log(secrets.weatherApiKey);
+// console.log(secrets.weatherApiKey);
 
 // Takes in either a Zip Code or City State (Hollywood CA)
-function getWeather(string) {
-  // Connect to API URL (https://teamtreehouse.com/username.json)
+function getWeather(query) {
+  // Connect to API URL
   const apiKey = '';
-  const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+  const request = https.get(`https://api.wunderground.com/api/${secrets.weatherApiKey}/geolookup/conditions/q/${query}.json`, response => {
     // Read the data
     var body = "";
-    response.on('data', data => {
-      body += data.toString();
+    response.on('data', chunk => {
+      body += chunk.toString();
     });
     // Response has finished
     response.on('end', () => {
       // Parsing = turning a string into a data structure (object)
       const weather = JSON.parse(body);
+      // console.log(weather);
       // Print the data
-      // These are fake data
-      printMessage(weather.loc, weather.temp);
+      printMessage(weather);
     });
   });
 }
 
-// getWeather('90201');
-
-// const locations = process.argv.slice(2);
-// locations.forEach(getWeather);
+// getWeather('95030');
+const locations = process.argv.slice(2);
+locations.forEach(getWeather);
