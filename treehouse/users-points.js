403c694903b6add05aefce22bdddff82244551c2
Fitzonreee <kevin.fitzhenry@createthenext.com>
2017-03-08 13:25:27 -0800

    Retrieving profile information for a list of Treehouse users

--- treehouse/users-points.js
+++ treehouse/users-points.js
@@ -1,5 +1,43 @@
-// Problem: We need a simple way to look at a user's badge count and JavaScript points
+// 'use strict';
+// node treehouse/users-points
 
+// Problem: We need a simple way to look at a user's badge count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information and print out
 
-console.log("Let's do this!");
+const https = require('https');
+// const username = 'kevinfitzhenry';
+
+// Function to print message to console
+function printMessage(username, badgeCount, points) {
+  const message = `${username} has ${badgeCount} badge(s) and ${points} total points`;
+  console.log(message);
+}
+// printMessage('kfitzhenry', 189, 23430);
+
+function getProfile(username) {
+  // Connect to API URL (https://teamtreehouse.com/username.json)
+  const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+    // Read the data
+    var body = "";
+    response.on('data', data => {
+      body += data.toString();
+    });
+    // Response has finished
+    response.on('end', () => {
+      // Parsing = turning a string into a data structure (object)
+      const profile = JSON.parse(body);
+      // Print the data
+      printMessage(username, profile.badges.length, profile.points.total);
+    });
+  });
+}
+
+// getProfile('kevinfitzhenry');
+// getProfile('alenaholligan');
+
+const users = ["kevinfitzhenry", "chalkers", "alenaholligan"];
+// users.forEach(username => {
+//   getProfile(username);
+// });
+// Or
+users.forEach(getProfile);
