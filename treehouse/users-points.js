eee1850087d191dbeacbf803d23aea51b0ed7803
Fitzonreee <kevin.fitzhenry@createthenext.com>
2017-03-10 08:21:52 -0800

    Added status codes for error message

--- treehouse/users-points.js
+++ treehouse/users-points.js
@@ -5,6 +5,8 @@
 // Solution: Use Node.js to connect to Treehouse's API to get profile information and print out
 
 const https = require('https');
+// Require just for status codes
+const http = require('http');
 // const username = 'kevinfitzhenry';
 
 // Print Error Messages
@@ -23,22 +25,28 @@ function getProfile(username) {
   try {
     // Connect to API URL (https://teamtreehouse.com/username.json)
     const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-      // Read the data
-      var body = "";
-      response.on('data', data => {
-        body += data.toString();
-      });
-      // Response has finished
-      response.on('end', () => {
-        try {
-          // Parsing = turning a string into a data structure (object)
-          const profile = JSON.parse(body);
-          // Print the data
-          printMessage(username, profile.badges.length, profile.points.total);
-        } catch (error) {
-          printError(error)
-        }
-      });
+      if (response.statusCode === 200) {
+        // Read the data
+        var body = "";
+        response.on('data', data => {
+          body += data.toString();
+        });
+        // Response has finished
+        response.on('end', () => {
+          try {
+            // Parsing = turning a string into a data structure (object)
+            const profile = JSON.parse(body);
+            // Print the data
+            printMessage(username, profile.badges.length, profile.points.total);
+          } catch (error) {
+            printError(error)
+          }
+        });
+      } else {
+        const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`
+        const statusCodeError = new Error(message);
+        printError(statusCodeError);
+      }
     });
     request.on('error', printError);
   } catch (error) {
